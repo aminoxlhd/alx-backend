34a617fe9388142d2d3266b2951cea20350293be
aminoxlhd <aminoxlhd@gmail.com>
2024-05-22 18:52:24 +0100

    edit 100-lfu_cache.py

--- 0x01-caching/100-lfu_cache.py
+++ 0x01-caching/100-lfu_cache.py
@@ -15,22 +15,22 @@ class LFUCache(BaseCaching):
         """put function"""
         if key or item is None:
             pass
-            if key in self.cache_data:
-                self.cache_data[key] = item
-                self.freq[key] += 1
-            else:
-                if len(self.cache_data) >= self.MAX_ITEMS:
-                    min_freq = min(self.freq.values())
-                    least_freq_keys = [
-                        k for k, v in self.freq.items() if v == min_freq
-                    ]
-                    lfu_key = min(least_freq_keys, key=self.freq.get)
-                    self.cache_data.pop(lfu_key)
-                    self.freq.pop(lfu_key)
-                    print("DISCARD:", lfu_key)
+        if key in self.cache_data:
+            self.cache_data[key] = item
+            self.freq[key] += 1
+        else:
+            if len(self.cache_data) >= self.MAX_ITEMS:
+                min_freq = min(self.freq.values())
+                least_freq_keys = [
+                    k for k, v in self.freq.items() if v == min_freq
+                ]
+                lfu_key = min(least_freq_keys, key=self.freq.get)
+                self.cache_data.pop(lfu_key)
+                self.freq.pop(lfu_key)
+                print("DISCARD:", lfu_key)
 
-                self.cache_data[key] = item
-                self.freq[key] = 1
+            self.cache_data[key] = item
+            self.freq[key] = 1
 
     def get(self, key):
         """get function"""
