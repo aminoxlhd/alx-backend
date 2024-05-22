f243c8b5eaa6b4c8120004ca63d324cd82b512a0
aminoxlhd <aminoxlhd@gmail.com>
2024-05-22 17:15:51 +0100

    add 3-lru_cache.py

---
+++ 0x01-caching/3-lru_cache.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""LRU Caching"""
+BaseCaching = __import__("base_caching").BaseCaching
+
+
+class LRUCache(BaseCaching):
+    """LRUCache class"""
+
+    def __init__(self):
+        """init function"""
+        super().__init__()
+
+    def put(self, key, item):
+        """put function"""
+        if key and item is None:
+            pass
+        else:
+            if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+                removed = self.order.pop(0)
+                self.cache_data.pop(removed)
+                print("DISCARD: {}".format(removed))
+            self.cache_data[key] = item
+            self.order.append(key)
+
+    def get(self, key):
+        """get function"""
+        if key in self.cache_data:
+            self.order.remove(key)
+            self.order.append(key)
+            return self.cache_data.get(key)
