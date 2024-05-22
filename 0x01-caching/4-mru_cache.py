c2dcd13398b88085d05c0cb66386de4faa256fbc
aminoxlhd <aminoxlhd@gmail.com>
2024-05-22 17:57:26 +0100

    add 4-mru_cache.py

---
+++ 0x01-caching/4-mru_cache.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""MRU Caching"""
+BaseCaching = __import__("base_caching").BaseCaching
+
+
+class MRUCache(BaseCaching):
+    """class MRUCache"""
+
+    def __init__(self):
+        """init function"""
+        super().__init__()
+        self.order = []
+
+    def put(self, key, item):
+        """put function"""
+        if key and item is None:
+            pass
+        else:
+            if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+                removed = self.order.pop()
+                self.cache_data.pop(removed)
+                print("DISCARD: {}".format(removed))
+            self.cache_data[key] = item
+            self.order.append(key)
+
+    def get(self, key):
+        """get function"""
+        if key in self.cache_data:
+            self.order.remove(key)
+            self.order.append(key)
+            return self.cache_data[key]
+        return None
