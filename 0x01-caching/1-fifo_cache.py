d12f8f905dcd449c6a8e92312f51cd4bfc40ce02
aminoxlhd <aminoxlhd@gmail.com>
2024-05-22 02:28:33 +0100

    edit 1-fifo_cache.py

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -13,17 +13,12 @@ class FIFOCache(BaseCaching):
     def put(self, key, item):
         """put function"""
         if key and item:
-            if key in self.cache_data:
+            if key and item:
+                if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+                    removed = next(iter(self.cache_data))
+                    self.cache_data.pop(removed)
+                    print("DISCARD: {}".format(removed))
                 self.cache_data[key] = item
-                return
-
-            if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
-                item_discarded = self.key_indexes.pop(0)
-                del self.cache_data[item_discarded]
-                print("DISCARD:", item_discarded)
-
-            self.cache_data[key] = item
-            self.key_indexes.append(key)
 
     def get(self, key):
         """get function"""
