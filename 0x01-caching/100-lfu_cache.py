26d2027867715bbbe459a6185d991507321934aa
aminoxlhd <aminoxlhd@gmail.com>
2024-05-23 01:22:45 +0100

    edit 100-lfu_cache.py

--- 0x01-caching/100-lfu_cache.py
+++ 0x01-caching/100-lfu_cache.py
@@ -20,8 +20,7 @@ class LFUCache(BaseCaching):
             if len(self.cache_data) >= self.MAX_ITEMS:
                 min_freq = min(self.freq.values())
                 least_freq_keys = [
-                    k for k, v in self.freq.items() if v == min_freq
-                ]
+                    k for k, v in self.freq.items() if v == min_freq]
                 lfu_key = min(least_freq_keys, key=self.freq.get)
                 self.cache_data.pop(lfu_key)
                 self.freq.pop(lfu_key)
