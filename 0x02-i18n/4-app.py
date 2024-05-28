6409b91f37712877c00912b6789bbc35ef677493
aminoxlhd <aminoxlhd@gmail.com>
2024-05-28 20:53:48 +0100

    add task 4

---
+++ 0x02-i18n/4-app.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+"""Force locale with URL parameter"""
+from flask import Flask, render_template, request
+from flask_babel import Babel
+
+app = Flask(__name__)
+babel = Babel(app)
+
+
+class Config(object):
+    """Config class"""
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app.config.from_object(Config)
+
+
+@babel.localeselector
+def get_locale():
+    """get_locale function"""
+    locale = request.args.get('locale')
+    if locale in Config.LANGUAGES:
+        return locale
+    return request.accept_languages.best_match(Config.LANGUAGES)
+
+
+@app.route("/")
+def index():
+    """index function"""
+    return render_template('4-index.html')
+
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port="5000")
