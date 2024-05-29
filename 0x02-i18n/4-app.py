b28c56eda59bbeb4fac12b1aedee830b28dd8725
aminoxlhd <aminoxlhd@gmail.com>
2024-05-29 03:47:45 +0100

    edit task 4

--- 0x02-i18n/4-app.py
+++ 0x02-i18n/4-app.py
@@ -1,36 +1,35 @@
 #!/usr/bin/env python3
 """Force locale with URL parameter"""
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, g
 from flask_babel import Babel
 
-app = Flask(__name__)
-babel = Babel(app)
 
-
-class Config(object):
+class Config:
     """Config class"""
     LANGUAGES = ["en", "fr"]
     BABEL_DEFAULT_LOCALE = "en"
     BABEL_DEFAULT_TIMEZONE = "UTC"
 
 
+app = Flask(__name__)
 app.config.from_object(Config)
+babel = Babel(app)
 
 
 @babel.localeselector
 def get_locale() -> str:
     """get_locale function"""
     locale = request.args.get('locale')
-    if locale in app.config["LANGUAGES"]:
+    if locale in app.config['LANGUAGES']:
         return locale
-    return request.accept_languages.best_match(app.config["LANGUAGES"])
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
-@app.route("/", methods=['GET'], strict_slashes=False)
+@app.route('/')
 def index() -> str:
     """index function"""
-    return render_template('4-index.html')
+    return render_template("4-index.html")
 
 
-if __name__ == '__main__':
-    app.run(host="0.0.0.0", port="5000")
+if __name__ == "__main__":
+    app.run(debug=True)
