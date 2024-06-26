878a3c9573ab780e18e254dbbdd8afe39f54e1de
aminoxlhd <aminoxlhd@gmail.com>
2024-06-26 18:50:16 +0000

    edit

---
+++ 0x03-queuing_system_in_js/0-redis_client.js
@@ -0,0 +1,22 @@
+import { createClient } from 'redis';
+
+const client = createClient();
+
+client.on('error', (err) => {
+  console.error('Redis client not connected to the server:', err.message);
+});
+
+client.on('connect', () => {
+  console.log('Redis client connected to the server');
+});
+
+(async () => {
+  try {
+    await client.connect();
+  } catch (error) {
+    console.error('Redis client not connected to the server:', error.message);
+  } finally {
+    await client.quit();
+  }
+})();
+
